f51f81b49c9e6699a3e2b27bb9d6aca6fd1de1b8
Torey Hickman <torey@toreyhickman.com>
2013-11-05 11:23:49 -0600

    Basic layout and styling

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,24 @@
 class PagesController < ApplicationController
 
   def welcome
-    @challenge = DBC::Challenge.find(1)
+    @cohorts = year_first(cohorts_in_phase_0)
+  end
+
+  private
+  def cohorts_in_phase_0
+    DBC::Cohort.all.select { |c| c.in_session == false && c.name.match(/2014/) && !c.name.match(/Melt/) }
+  end
+
+  def year_first(cohorts_array)
+    cohorts = cohorts_array.each do |c|
+      puts c.name
+      c.instance_variable_set(:@name, c.name.sub(cohort_name_regex, '\3 \2 \1'))
+    end
+    cohorts
+  end
+
+  def cohort_name_regex
+    /(\w+\s?\w+)(\s)(2014)/
   end
 
 end
