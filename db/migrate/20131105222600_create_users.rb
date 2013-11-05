092e3bed689d2124c2c2ec696b494209346c9855
Torey Hickman <torey@toreyhickman.com>
2013-11-05 17:14:34 -0600

    User added

---
+++ db/migrate/20131105222600_create_users.rb
@@ -0,0 +1,20 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table  :users do |t|
+      t.integer   :socrates_id
+      t.integer   :cohort_id
+      t.string    :name
+      t.string    :email
+      t.string    :github
+      t.string    :twitter
+      t.string    :blog_url
+      t.text      :bio
+      t.boolean   :intellectual_flag, default: false
+      t.boolean   :cultural_flag, default: false
+
+      t.timestamps
+    end
+
+    add_index :users, :cohort_id
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131105193649) do
+ActiveRecord::Schema.define(version: 20131105222600) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,5 +26,22 @@ ActiveRecord::Schema.define(version: 20131105193649) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "users", force: true do |t|
+    t.integer  "socrates_id"
+    t.integer  "cohort_id"
+    t.string   "name"
+    t.string   "email"
+    t.string   "github"
+    t.string   "twitter"
+    t.string   "blog_url"
+    t.text     "bio"
+    t.boolean  "intellectual_flag", default: false
+    t.boolean  "cultural_flag",     default: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "users", ["cohort_id"], name: "index_users_on_cohort_id", using: :btree
 
 end
