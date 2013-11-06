32ff03178052820f7e610d1a6e7fffc2c4217b7c
Torey Hickman <torey@toreyhickman.com>
2013-11-06 10:06:15 -0600

    Seed data for cohorts and users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,33 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# Seed all of the cohorts
+cohorts = DBC::Cohort.all
+
+cohorts.each do |c|
+  cohort = Cohort.find_or_initialize_by(socrates_id: c.id)
+
+  cohort.socrates_id = c.id unless cohort.socrates_id
+  cohort.name = c.name
+  cohort.email = c.email
+  cohort.location = c.location
+  cohort.start_date = c.start_date
+  cohort.in_session = c.in_session
+
+  cohort.save
+end
+
+# Seed all users
+users = DBC::User.all
+
+users.each do |u|
+  user = User.find_or_initialize_by(socrates_id: u.id)
+
+  user.socrates_id = u.id unless user.socrates_id
+  user.cohort_id = u.cohort_id
+  user.name = u.name
+  user.email = u.email
+  user.github = u.profile[:github]
+  user.twitter = u.profile[:twitter]
+  user.blog_url = u.profile[:blog]
+  user.bio = u.bio
+
+  user.save
+end
