45f8bbd9ef4baa292c1663bf02083b3729d78857
Torey Hickman <torey@toreyhickman.com>
2013-11-07 11:13:45 -0600

    Gravatar images added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
+  include Gravtastic
+  gravtastic secure: true, filetype: :jpg, size: 100#, default: "https://secure.gravatar.com/avatar/de91aca465cb595e94fe2c1d60bb2ae8.png?r=PG&d=mm&s=100"
+
+
   belongs_to :cohort
 end
