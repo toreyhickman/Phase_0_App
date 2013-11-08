c120782f52d9af24a529eb496578f9aa4de1475d
Torey Hickman <torey@toreyhickman.com>
2013-11-08 17:39:14 -0600

    User show progress

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
 
   def show
+    @student = User.includes(:cohort, :exercise_attempts, :attempted_exercises).find(params[:id])
+    @exercises = Exercise.all.sort
+
+    @attempts_by_exercise_id = @student.exercise_attempts.group_by(&:exercise_id)
   end
 end
