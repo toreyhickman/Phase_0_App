a1b98c0fa34fbcc4553904b655dc4407a015bc89
Torey Hickman <torey@toreyhickman.com>
2013-11-11 15:32:47 -0600

    User show include challenge attempts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,12 @@
 class UsersController < ApplicationController
 
   def show
-    @student = User.includes(:cohort, :exercise_attempts, :attempted_exercises).find(params[:id])
-    @exercises = Exercise.all.sort
+    @student = User.includes(:cohort, :exercise_attempts, :attempted_exercises, :challenge_attempts, :attempted_challenges).find(params[:id])
 
+    @exercises = Exercise.all.sort
     @attempts_by_exercise_id = @student.exercise_attempts.group_by(&:exercise_id)
+
+    @weeks = Week.includes(:challenges).where("id <= ?", @student.cohort.current_week)
+    @attempts_by_challenge_id = @student.challenge_attempts.group_by(&:challenge_id)
   end
 end
