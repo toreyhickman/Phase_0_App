76f519aa2d7d89846321ef1eceb2ac858023c121
Torey Hickman <torey@toreyhickman.com>
2013-11-12 16:43:38 -0600

    Mail_to links enhanced

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,10 @@ module ApplicationHelper
   def current_user
     @current_user ||= User.find(session[:socrates_id]) if session[:socrates_id]
   end
+
+  def set_email_cc(location)
+    return "chicago.prep@devbootcamp.com" if location == "Chicago"
+    return "sf.prep@devbootcamp.com" if location == "San Francisco"
+    return ""
+  end
 end
