66e32d23d014c46b237b6b8a3d379081392fcc01
Torey Hickman <torey@toreyhickman.com>
2013-12-12 10:47:47 -0600

    Pull students' challenge and exercise attempt data for Cohort show view
    This data will be needed to calculate a last active date for each student.

--- app/controllers/cohorts_controller.rb
+++ app/controllers/cohorts_controller.rb
@@ -5,7 +5,7 @@ class CohortsController < ApplicationController
   end
 
   def show
-    @cohort = Cohort.includes(students: [:attempted_exercises, :attempted_challenges]).find(params[:id])
+    @cohort = Cohort.includes(students: [:attempted_exercises, :attempted_challenges, :challenge_attempts, :exercise_attempts]).find(params[:id])
     @challenges_due = Week.includes(:challenges).where("id < ?", @cohort.current_week).map(&:challenges).flatten
     @exercises_due = Exercise.all
   end
